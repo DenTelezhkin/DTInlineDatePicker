66858bc91518538a2e8933b39f55a9e1bee1ebba
Denys Telezhkin <strangervir@gmail.com>
2014-02-27 17:39:08 +0200

    add documentation

--- DTInlineDatePicker/DTInlinePickerCell.h
+++ DTInlineDatePicker/DTInlinePickerCell.h
@@ -6,8 +6,15 @@
 #import <Foundation/Foundation.h>
 #import "DTTableViewCell.h"
 
+/**
+ DTInlinePickerCell is intended to be used with DTDatePickerPresenter class. It automatically manages presentation of UIDatePicker inside the cell.
+ */
+
 @interface DTInlinePickerCell : DTTableViewCell
 
+/**
+ UIDatePicker object, that is presented.
+ */
 @property (nonatomic, strong) UIDatePicker * pickerView;
 
 @end
--- DTInlineDatePicker/DTInlinePickerTableController.h
+++ DTInlineDatePicker/DTInlinePickerTableController.h
@@ -8,13 +8,35 @@
 
 #import "DTTableViewController.h"
 
+/**
+ DTInlinePickerTableController allows easily attach inline date picker behaviour to any UITableViewCell. It requires using DTTableViewManager and DTPickerPresenter libraries to work.
+ */
 @interface DTInlinePickerTableController : DTTableViewController
 
--(void)attachInlinePickerPresenter:(id)presenter toIndexPath:(NSIndexPath *)indexPath;
+/**
+ Attaching inline picker to indexPath. UIDatePicker cell(DTInlinePickerCell) will be inserted under this indexPath. Keep in mind, picker will not show until this indexPath get's selected by the user. Good place to call this method is viewDidLoad method.
+ 
+ @param presenter date picker presenter object
+ 
+ @param indexPath indexPath of the cell, that contains some date information.
+ */
+-(void)attachInlinePickerPresenter:(DTDatePickerPresenter *)presenter toIndexPath:(NSIndexPath *)indexPath;
 
+/**
+ Detach inline picker from indexPath. This method does not remove date picker cell, if it is presented.
+ 
+ @param indexPath indexPath of cell, that no longer needs UIDatePicker
+ */
 -(void)detachInlinePickerPresenterFromIndexPath:(NSIndexPath * )indexPath;
 
+/**
+ Implement this method, if you want to be notified when date picker will be shown. Default implementation of this method does nothing.
+ */
 -(void)didShowPickerForIndexPath:(NSIndexPath * )indexPath;
+
+/**
+ Implement this method, if you want to be notified when date picker will be hidden. Default implementation of this method does nothing.
+ */
 -(void)didHidePickerForIndexPath:(NSIndexPath * )indexPath;
 
 @end
